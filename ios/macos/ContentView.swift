c98d4be6e0e8e122bb2783f523664d032042e5c7
jado <j@jadon.io>
2022-08-08 09:37:44 -0700

    Generate random boards

--- ios/macos/ContentView.swift
+++ ios/macos/ContentView.swift
@@ -1,11 +1,48 @@
 import SwiftUI
 import shared
 
+struct Test {
+    func test() {
+        // we can call a method on a kotlin singleton!
+        SeaTile.shared.giveResource()
+        
+    }
+}
+
 struct ContentView: View {
     let greet = Greeting().greeting()
+    let tiles = 5
+    let board = ModelKt.generateBoard(size: 5)
 
     var body: some View {
-        Text(greet)
+        VStack {
+            Text(greet).padding()
+
+            GeometryReader { geometry in
+                let windowWidth = geometry.size.width
+                let windowHeight = geometry.size.height
+                let tileSize = min(windowWidth, windowHeight) / CGFloat(tiles)
+                let hexagonWidth: CGFloat = (tileSize / 2) * cos(.pi / 6) * 2
+                let spacing: CGFloat = 0
+                
+                ForEach(0..<tiles, id: \.self) { y in
+                    ForEach(0..<tiles, id: \.self) { x in
+                        let offsetX = hexagonWidth * CGFloat(x % tiles)
+                         + (y % 2 == 0 ? 0 : hexagonWidth / 2 + (spacing/2))
+                        let offsetY = (tileSize * 0.75) * CGFloat(y % tiles)
+                        Image(getId(x: x, y: y))
+                            .resizable()
+                            .clipShape(PolygonShape(sides: 6).rotation(Angle.degrees(90)))
+                            .frame(width: tileSize, height: tileSize)
+                            .offset(x: offsetX, y: offsetY)
+                    }
+                }
+            }
+        }
+    }
+    
+    func getId(x: Int, y: Int) -> String {
+        return board.tiles.get(index: Int32(y))?.get(index: Int32(x))?.id ?? "desert"
     }
 }
 
--- shared/src/commonMain/kotlin/io/jadon/metan/model/model.kt
+++ shared/src/commonMain/kotlin/io/jadon/metan/model/model.kt
@@ -1,2 +1,61 @@
 package io.jadon.metan.model
 
+import kotlin.random.Random
+
+interface Resource
+
+object WheatResource : Resource
+object SheepResource : Resource
+object RockResource : Resource
+object WoodResource : Resource
+object BrickResource : Resource
+
+interface Tile {
+    val id: String
+    fun giveResource(): List<Resource> = listOf()
+}
+
+object FieldTile : Tile {
+    override val id: String = "field"
+}
+
+object PastureTile : Tile{
+    override val id: String = "pasture"
+}
+object MountainTile : Tile {
+    override val id: String = "mountain"
+}
+object ForestTile : Tile {
+    override val id: String = "forest"
+}
+object HillTile : Tile {
+    override val id: String = "hill"
+}
+object DesertTile : Tile {
+    override val id: String = "desert"
+}
+object SeaTile : Tile {
+    override val id: String = "sea"
+}
+
+class TileGrid(val size: Int = 5) {
+    val tiles = Array(size) { arrayOfNulls<Tile>(size) }
+}
+
+fun generateBoard(size: Int = 5): TileGrid {
+    val possibleTiles = listOf(
+        FieldTile,
+        PastureTile,
+        MountainTile,
+        ForestTile,
+        HillTile,
+        DesertTile,
+    )
+    return TileGrid(size).apply {
+        (0 until size).forEach { y ->
+            (0 until size).forEach { x ->
+                tiles[y][x] = possibleTiles[Random.Default.nextInt(possibleTiles.size)]
+            }
+        }
+    }
+}
